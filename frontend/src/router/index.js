59f62bb82d5ce64b9012eed63b4f56bad6846e62
ziyouuu <ziyou.foo.2022@scis.smu.edu.sg>
2024-09-26 16:59:59 +0800

    revert back to commit beb47218ac89a3d2db6cc83fc9e3f58a91495200

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
-import DashboardView from '../views/DashboardView.vue'
 import ScheduleView from '../views/ScheduleView.vue'
 import RequestView from '@/views/RequestView.vue'
-import ApplicationView from '@/views/ApplicationView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,9 +12,6 @@ const router = createRouter({
       component: LoginView
     },
     {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: DashboardView,
       path: '/schedule',
       name: 'schedule',
       component: ScheduleView,
@@ -28,12 +23,6 @@ const router = createRouter({
       component: RequestView,
       meta: { requiresAuth: true }
     },
-
-    {
-      path: '/application',
-      name: 'application',
-      component: ApplicationView
-    }
     // ... other routes ...
   ]
 })
@@ -41,7 +30,6 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!localStorage.getItem('user')) {
-      next('/login')
       next('/')
     } else {
       next()
@@ -50,3 +38,5 @@ router.beforeEach((to, from, next) => {
     next()
   }
 })
+
+export default router
